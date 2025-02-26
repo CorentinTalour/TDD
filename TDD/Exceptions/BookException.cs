3126f2b648e2d6cb1c92abd060b89ec21b4ff677
Corentin Talour <corentintalour@gmail.com>
2025-02-26 16:38:21 +0100

    Ajout de la gestion des exceptions pour la validation des ISBN dans la classe BookService

---
+++ TDD/Exceptions/BookException.cs
@@ -0,0 +1,25 @@
+namespace TDD.Exceptions;
+
+public class IsbnLengthException : Exception
+{
+    public IsbnLengthException()
+        : base("L'ISBN doit contenir exactement 10 caractères.")
+    {
+    }
+}
+
+public class IsbnFormatException : Exception
+{
+    public IsbnFormatException()
+        : base("L'ISBN ne doit contenir que des chiffres ou un X pour la clé.")
+    {
+    }
+}
+
+public class IsbnKeyException : Exception
+{
+    public IsbnKeyException()
+        : base("La clé de l'ISBN doit être un chiffre ou un X.")
+    {
+    }
+}
