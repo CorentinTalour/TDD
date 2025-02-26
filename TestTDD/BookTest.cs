1247eefa77e62b2fc064d668d971eb462f14383d
Corentin Talour <corentintalour@gmail.com>
2025-02-26 16:43:12 +0100

    Refactor unit tests for ISBN validation in BookTest class to use DataRow attributes for multiple test cases

--- TestTDD/BookTest.cs
+++ TestTDD/BookTest.cs
@@ -6,53 +6,72 @@ namespace TestTDD;
 [TestClass]
 public class BookTest
 {
-    [TestMethod]
-    public void whenIsbn10IsValid_shouldReturnTrue()
+    [DataTestMethod]
+    [DataRow("2970154706")]
+    [DataRow("3455503950")]
+    [DataRow("2012036961")]
+    [DataRow("080442957X")]
+    //Test avec tiret
+    [DataRow("2-970154706")]
+    [DataRow("345550395-0")]
+    [DataRow("201-2036961")]
+    [DataRow("0-8044-2957-X")]
+    //Test avec espace
+    [DataRow("2 970154706")]
+    [DataRow("345550 3950")]
+    [DataRow("2012 036961")]
+    [DataRow("08 04429 57 X")]
+    public void whenIsbn10IsValid_shouldReturnTrue(string isbn)
     {
-        BookService service = new BookService();
-        string isbn = "2970154706";
-        
-        bool result = service.VerifierISBN(isbn);
-        
+        var service = new BookService();
+
+        var result = service.VerifierISBN(isbn);
+
         Assert.IsTrue(result);
     }
 
-    [TestMethod]
-    public void whenIsbn10IsNotValid_shouldReturnFalse()
+    [DataTestMethod]
+    [DataRow("2970154707")]
+    [DataRow("3455503959")]
+    [DataRow("2012036968")]
+    [DataRow("201203696X")]
+    public void whenIsbn10IsNotValid_shouldReturnFalse(string isbn)
     {
-        BookService service = new BookService();
-        string isbn = "2970154701";
-        
-        bool result = service.VerifierISBN(isbn);
-        
+        var service = new BookService();
+
+        var result = service.VerifierISBN(isbn);
+
         Assert.IsFalse(result);
     }
 
-    [TestMethod]
-    public void whenIsbn10NotContains10Digits_shouldReturnIsbnLengthException()
+    [DataTestMethod]
+    [DataRow("297015470")]
+    [DataRow("29701547081")]
+    public void whenIsbn10NotContains10Digits_shouldReturnIsbnLengthException(string isbn)
     {
-        BookService service = new BookService();
-        string isbn = "297015406";
-        
+        var service = new BookService();
+
         Assert.ThrowsException<IsbnLengthException>(() => service.VerifierISBN(isbn));
     }
 
-    [TestMethod]
-    public void whenIsbn10ContainsLetter_shouldReturnIsbnFormatException()
+    [DataTestMethod]
+    [DataRow("297R154701")]
+    [DataRow("29d015g706")]
+    [DataRow("A2901570q6")]
+    public void whenIsbn10ContainsLetter_shouldReturnIsbnFormatException(string isbn)
     {
-        BookService service = new BookService();
-        string isbn = "297015h706";
-        
+        var service = new BookService();
+
         Assert.ThrowsException<IsbnFormatException>(() => service.VerifierISBN(isbn));
     }
 
-    [TestMethod]
-    public void whenIsbn10ContainsKeyLetter_shouldReturnIsbnKeyException()
+    [DataTestMethod]
+    [DataRow("297015470R")]
+    [DataRow("345550395j")]
+    public void whenIsbn10ContainsKeyLetter_shouldReturnIsbnKeyException(string isbn)
     {
-        BookService service = new BookService();
-        string isbn = "297015470h";
-        
+        var service = new BookService();
+
         Assert.ThrowsException<IsbnKeyException>(() => service.VerifierISBN(isbn));
     }
-
 }
