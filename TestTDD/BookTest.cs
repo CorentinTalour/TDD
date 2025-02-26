17655ba4643ed42345177d4113866120bbf8a68e
Corentin Talour <corentintalour@gmail.com>
2025-02-26 16:36:10 +0100

    Ajout de tests unitaires pour la validation des exceptions associées

--- TestTDD/BookTest.cs
+++ TestTDD/BookTest.cs
@@ -26,4 +26,32 @@ public class BookTest
         
         Assert.IsFalse(result);
     }
+
+    [TestMethod]
+    public void whenIsbn10NotContains10Digits_shouldReturnIsbnLengthException()
+    {
+        BookService service = new BookService();
+        string isbn = "297015406";
+        
+        Assert.ThrowsException<IsbnLengthException>(() => service.VerifierISBN(isbn));
+    }
+
+    [TestMethod]
+    public void whenIsbn10ContainsLetter_shouldReturnIsbnFormatException()
+    {
+        BookService service = new BookService();
+        string isbn = "297015h706";
+        
+        Assert.ThrowsException<IsbnFormatException>(() => service.VerifierISBN(isbn));
+    }
+
+    [TestMethod]
+    public void whenIsbn10ContainsKeyLetter_shouldReturnIsbnKeyException()
+    {
+        BookService service = new BookService();
+        string isbn = "297015470h";
+        
+        Assert.ThrowsException<IsbnKeyException>(() => service.VerifierISBN(isbn));
+    }
+
 }
